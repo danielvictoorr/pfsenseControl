9dc0b766037f18c47299ba457b3b5f38b61b6915
danielvictoorr <danielvictor1408@gmail.com>
2024-12-08 14:20:09 -0300

    feat: subindo tela do firewall

---
+++ pfsenseControl/resources/views/firewall.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('title', 'Firewall')
+
+@section('content')
+<div class="container">
+    <h1 class="mt-3">Gerenciamento de Firewall</h1>
+    <p>Bem-vindo à página de gerenciamento de configurações do Firewall.</p>
+</div>
+@endsection
--- pfsenseControl/resources/views/partials/sidebar.blade.php
+++ pfsenseControl/resources/views/partials/sidebar.blade.php
@@ -19,7 +19,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="{{ route('firewall') }}" class="nav-link">
                         <i class="nav-icon"></i>
                         <p>Firewall</p>
                     </a>
