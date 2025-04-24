7ad6b036580c8bd2afe169c3611586045f31ba3d
danielvictoorr <danielvictor1408@gmail.com>
2025-04-23 22:46:47 -0300

    fix: ajustando chamada paraa api  externa do pfsense

--- pfsenseControl/app/Http/Controllers/FirewallController.php
+++ pfsenseControl/app/Http/Controllers/FirewallController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Firewall;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\Log;
@@ -12,45 +13,9 @@ class FirewallController extends Controller
     public function index()
     {
         try {
-            // Configuração do cliente Guzzle
-            $client = new Client([
-                'base_uri' => getenv('services.external_api.url'),
-                'verify' => false, // Desativa a verificação SSL (não recomendado para produção)
-                'auth' => ['admin', '1234'], // Autenticação básica
-                'headers' => [
-                    'x-api-key' => getenv('API_KEY'),
-                ],
-            ]);
+            $firewallRules = new Firewall();
 
-            $response = $client->get('/api/v2/firewall/rules', [
-                'query' => [
-                    'limit' => 0,
-                    'offset' => 0,
-                ],
-            ]);
-
-                      // Debugando a resposta
-                      $statusCode = $response->getStatusCode();
-                      $responseBody = $response->getBody()->getContents();
-                      $responseHeaders = $response->getHeaders();
-          
-                      // Log de debug
-                      Log::info('Requisição API bem-sucedida', [
-                          'status' => $statusCode,
-                          'headers' => $responseHeaders,
-                          'body' => $responseBody,
-                      ]);
-
-            // Verifica o status da resposta
-            if ($response->getStatusCode() === 200) {
-                $data = json_decode($response->getBody(), true); // Decodifica o JSON para um array associativo
-                $firewallRules = $data['data'] ?? []; // Captura os dados ou define um array vazio
-            } else {
-                $firewallRules = []; // Define um array vazio se a resposta não for bem-sucedida
-            }
-
-            // Retorna a view com os dados
-            return view('firewall', ['firewallRules' => $firewallRules]);
+            return view('firewall', ['firewallRules' => $firewallRules->getRules()]);
         } catch (RequestException $e) {
             // Captura erros de requisição
             return view('firewall', [
@@ -67,20 +32,13 @@ class FirewallController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function insertRules(object $data)
     {
-        // Aqui você pode validar os dados
-        $data = $request->validate([
-            'type' => 'required|string',
-            'ipprotocol' => 'required|string',
-            'interface' => 'required|array',
-            'source' => 'required|string',
-            'destination' => 'required|string',
-            'descr' => 'nullable|string',
-        ]);
+        try{
 
-        // Aqui você faria a lógica para salvar no banco ou enviar para o pfSense, etc.
-        // Por enquanto, vamos só retornar para a tela com sucesso:
-        return redirect()->back()->with('success', 'Regra adicionada com sucesso!');
+        }
+        catch (\Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
--- pfsenseControl/app/Services/Firewall.php
+++ pfsenseControl/app/Services/Firewall.php
@@ -3,19 +3,62 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class Firewall extends ExternalApiService
 {
-
-    public function getRules()
+    private $client;
+    public function __construct() {
+        $this->client = new Client([
+            'base_uri' => getenv('services.external_api.url'),
+            'verify' => false, // Desativa a verificação SSL (não recomendado para produção)
+            'auth' => ['admin', '1234'], // Autenticação básica
+            'headers' => [
+                'x-api-key' => getenv('API_KEY'),
+            ],
+        ]);
+    }
+    public function getRules() :array
     {
-        $endPoint = '/api/v2/firewall/rules?limit=0&offset=0';
 
-        $response = self::getData($endPoint);
+        $response = $this->client->get('/api/v2/firewall/rules', [
+            'query' => [
+                'limit' => 0,
+                'offset' => 0,
+            ],
+        ]);
 
-        return $response;
+                  // Debugando a resposta
+                  $statusCode = $response->getStatusCode();
+                  $responseBody = $response->getBody()->getContents();
+                  $responseHeaders = $response->getHeaders();
+      
+                  // Log de debug
+                  Log::info('Requisição API bem-sucedida', [
+                      'status' => $statusCode,
+                      'headers' => $responseHeaders,
+                      'body' => $responseBody,
+                  ]);
+
+        // Verifica o status da resposta
+        if ($response->getStatusCode() === 200) {
+            $data = json_decode($response->getBody(), true); // Decodifica o JSON para um array associativo
+            $firewallRules = $data['data'] ?? []; // Captura os dados ou define um array vazio
+        } else {
+            $firewallRules = []; // Define um array vazio se a resposta não for bem-sucedida
+        }
+
+        return $firewallRules;
     }
 
+
+    public function insertRules(object $rulesInformation)
+    {
+        
+    }
 
 
 }
